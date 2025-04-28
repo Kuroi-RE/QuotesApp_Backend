25a4f91d5759ea0fbd07c0b12aa9e87041ce38c7
Kelvin Ferdinan <91706611+mispipin@users.noreply.github.com>
2025-04-28 19:23:12 +0700

    fungsi load_quotes

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
 import json
 
+def load_quotes(path='quotes.json'):
+    with open(path, 'r', encoding='utf-8') as file:
+        return json.load(file)
+        
 def filter_by_lang(quotes, lang):
     return [q for q in quotes if q['lang'] == lang]
